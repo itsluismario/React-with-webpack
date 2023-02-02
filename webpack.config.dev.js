f31f2a499e88063659b8daf0841e54f5fa9531ad
Luis Mario <itsluismario@gmail.com>
2023-02-02 11:25:55 -0600

    :heavy_plus_sign: [UPDATE] Css-minimizer, terser and clean

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimiezerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles': path.resolve(__dirname, 'src/styles/')
-    }
+    extensions: ['.js', '.jsx']
   },
-  mode: 'production',
+  mode: 'development',
   module: {
     rules: [
       {
@@ -52,14 +44,14 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
         filename: '[name].css'
-    }), 
-    new CleanWebpackPlugin(),
+    })
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimiezerPlugin(),
-      new TerserPlugin(),
-    ]
+  devServer: {
+    static: {
+            directory: path.join(__dirname, 'dist'),
+    },
+    watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+    compress: true,
+    port: 3006
   }
 }
